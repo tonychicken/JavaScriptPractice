a0f2035136898324faa7f4688397aa7c738cada8
tonychicken <abcdqazzaq@gmail.com>
2023-03-02 22:05:35 +0800

    w01-1

---
+++ W01/js/app.js
@@ -0,0 +1,19 @@
+const number = document.querySelector('#input-number');
+const add = document.querySelector('#btn-add');
+const subtract = document.querySelector('#btn-subtract');
+const multiply = document.querySelector('#btn-multiply');
+const divide = document.querySelector('#btn-divide');
+
+
+
+const calculation = document.querySelector('#current-calculation');
+const result = document.querySelector('#current-result');
+
+
+console.log(number,"number");
+console.log(add,"add");
+console.log(subtract,"subtract");
+console.log(multiply,"multiply");
+console.log(divide,"divide");
+console.log(calculation,"calculation");
+console.log(result,"result");
