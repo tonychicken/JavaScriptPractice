f936a34a9adae432cc8c06a94199771d7df0cb14
tonychicken <abcdqazzaq@gmail.com>
2023-03-09 23:29:51 +0800

    w04menustart

---
+++ W02/tictactoe_06.js
@@ -0,0 +1,82 @@
+const reset_btn = document.querySelector('#reset');
+const container = document.querySelector('#container');
+
+const allLi = document.querySelectorAll('#board li');
+let checkTurn = 0;
+console.log(allLi)
+const checkwin = (player) => {
+
+    let p = [];
+    allLi.forEach((item) => {
+        p.push(item.classList.contains(player))
+    });
+    console.log(p)
+    const [p1, p2, p3, p4, p5, p6, p7, p8, p9] = p;
+
+    console.log(p)
+
+    if (
+        (p1&&p2&p3)||
+        (p4&&p5&&p6)||
+        (p7&&p8&&p9)||
+        (p1&&p5&&p9)||
+        (p3&&p5&&p7)||
+        (p1&&p3&p5)||
+        (p2&&p4&p6)||
+        (p3&&p5&p9))
+    {
+        return sop(player);
+    } 
+    else
+    {
+        return false;
+    }
+
+
+}
+const sop=(player)=>{
+    if(player='o'){
+        container.style.backgroundColor="green"
+        console.log(player+"贏了")
+        alert(player+"贏了")
+    }else{
+        container.style.backgroundColor="red"
+        console.log(player+"贏了")
+        alert(player+"贏了")
+    }
+}
+
+allLi.forEach((item) => {
+    item.addEventListener('click', () => {
+        if (item.classList.contains('disable')) {
+            alert("already feiled")
+        } else if (checkTurn % 2 === 0) {
+            item.textContent = 'o';
+            item.classList = 'o disable';
+        } else if (checkTurn % 2 === 1) {
+            item.textContent = 'x';
+            item.classList = 'x disable';
+        }
+
+        if (checkTurn < 8) {
+            checkTurn++
+        }
+        console.log(item.textContent)
+        checkwin(item.textContent)
+    })
+});
+
+
+const reset = () => {
+    allLi.forEach((item) => {
+        checkTurn=0;
+        container.style.backgroundColor="#666"
+        item.textContent = '+';
+        item.classList = '';
+        console.log(checkTurn)
+    });
+}
+
+console.log('checkwin(o)', checkwin('o'))
+console.log('checkwin(x)', checkwin('x'))
+reset_btn.addEventListener('click', reset);
---
+++ W04/app_06.js
@@ -0,0 +1,124 @@
+const menu = [
+    {
+      id: 1,
+      title: "buttermilk pancakes",
+      category: "breakfast",
+      price: 15.99,
+      img: "./images/06001.jpeg",
+      desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    },
+    {
+      id: 2,
+      title: "diner double",
+      category: "lunch",
+      price: 13.99,
+      img: "./images/06002.jpeg",
+      desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    },
+    {
+      id: 3,
+      title: "godzilla milkshake",
+      category: "dinner",
+      price: 6.99,
+      img: "./images/06003.jpeg",
+      desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    },
+    {
+      id: 4,
+      title: "country delight",
+      category: "breakfast",
+      price: 20.99,
+      img: "./images/item-4.jpeg",
+      desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    },
+    {
+      id: 5,
+      title: "egg attack",
+      category: "lunch",
+      price: 22.99,
+      img: "./images/item-5.jpeg",
+      desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    },
+    {
+      id: 6,
+      title: "oreo dream",
+      category: "shakes",
+      price: 18.99,
+      img: "./images/item-6.jpeg",
+      desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    }
+  ];
+  
+  
+  window.addEventListener('DOMContentLoaded',()=>{
+    displayMenuItems(menu);
+    displayMenuButtons();
+  })
+  
+  const sectionCenter=document.querySelector('.section-center');
+  const btnContainer = document.querySelector('.btn-container');
+  
+  const displayMenuItems=(menuItems)=>{
+    let displayMenu=menuItems.map((item)=>{
+      console.log('item',item);
+  
+      return`
+      </article>
+      <article class="menu-item">
+        <img src="${item.img}" alt="menu item"class="photo">
+        <div class="item-info">
+          <header>
+            <h4>${item.title}</h4>
+            <h4 class="price">${item.price}</h4>
+          </header>
+          <p class="item-text">
+            ${item.desc}
+          </p>
+        </div>
+      </article>  
+      `
+    })
+    displayMenu=displayMenu.join("");
+  
+    console.log('displayMenu',displayMenu);
+  
+    sectionCenter.innerHTML = displayMenu;
+  }
+  
+  const displayMenuButtons=()=>{
+    const categories = menu.reduce((values,item)=>{
+      if(!values.includes(item.category)){
+        values.push(item.category);
+      }
+      return values;
+    },['all']);
+    //console.log('categories',categories);
+  
+    const categoryBtns=categories.map((category)=>{
+      return `<button type="button" class="filter-btn" data-id=${category}>${category}</button>`
+    })
+    .join('');
+  
+    btnContainer.innerHTML=categoryBtns;
+  
+  
+    const filterBtns=btnContainer.querySelectorAll('.filter-btn')
+    console.log('filter-btn',filterBtns);
+    filterBtns.forEach((btn)=>{
+      btn.addEventListener('click',(e)=>{
+        console.log('data-id',e.currentTarget.dataset.id);
+        const category=e.currentTarget.dataset.id;
+        const menuCategory= menu.filter((menuItem)=>{
+          if(menuItem.category===category){
+            return menuItem;
+          }
+        });
+        if(category =='all'){
+          displayMenuItems(menu);
+        }else{
+          displayMenuItems(menuCategory);
+        }
+      });
+    });
+  }
+  
