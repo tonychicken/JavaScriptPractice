94ef1b74e157499f5706789d9239962ce1a5b019
tonychicken <abcdqazzaq@gmail.com>
2023-03-16 23:28:56 +0800

    w05p1amdp2

--- W04/app_06.js
+++ W04/app_06.js
@@ -1,55 +1,5 @@
-const menu = [
-    {
-      id: 1,
-      title: "buttermilk pancakes",
-      category: "breakfast",
-      price: 15.99,
-      img: "./images/06001.jpeg",
-      desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
-    },
-    {
-      id: 2,
-      title: "diner double",
-      category: "lunch",
-      price: 13.99,
-      img: "./images/06002.jpeg",
-      desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
-    },
-    {
-      id: 3,
-      title: "godzilla milkshake",
-      category: "dinner",
-      price: 6.99,
-      img: "./images/06003.jpeg",
-      desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
-    },
-    {
-      id: 4,
-      title: "country delight",
-      category: "breakfast",
-      price: 20.99,
-      img: "./images/item-4.jpeg",
-      desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
-    },
-    {
-      id: 5,
-      title: "egg attack",
-      category: "lunch",
-      price: 22.99,
-      img: "./images/item-5.jpeg",
-      desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
-    },
-    {
-      id: 6,
-      title: "oreo dream",
-      category: "shakes",
-      price: 18.99,
-      img: "./images/item-6.jpeg",
-      desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
-    },
-  ];
-  
-  
+import { menu } from "./data.js";
+
   window.addEventListener('DOMContentLoaded',()=>{
     displayMenuItems(menu);
     displayMenuButtons();
@@ -113,8 +63,10 @@ const menu = [
         });
         if(category =='all'){
           displayMenuItems(menu);
+          console.log("filterMenu",menu);
         }else{
           displayMenuItems(menuCategory);
+          console.log("filterMenu",menuCategory);
         }
       });
     });
