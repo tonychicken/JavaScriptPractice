aead07aea44ffd019afb6e3da225dab9979b5168
tony.zhan <tony.zhan@shinda.com.tw>
2023-06-12 02:14:33 +0800

    vercelUpdate2

--- midterm/app.js
+++ midterm/app.js
@@ -77,89 +77,134 @@
 const ClassdemoOpenBtn = document.querySelector('#navbarDropdownClassDemo');
 const ClassDemo = document.querySelector('#dropdown_ClassDemo');
 console.log(ClassDemo)
-ClassdemoOpenBtn.addEventListener('click',()=>{
-  if(ClassDemo.classList.contains('show')){
+ClassdemoOpenBtn.addEventListener('click', () => {
+  if (ClassDemo.classList.contains('show')) {
     ClassDemo.classList.remove("show")
-  }else{
+  } else {
     ClassDemo.classList.add("show");
   }
 });
 
 const gitlink = document.querySelectorAll('.nav-link');
-gitlink.forEach((item)=>{
-if(item.classList.contains('github')){
-  item.addEventListener('click', () => {
-    window.open('https://github.com/tonychicken/JavaScriptPractice.git');
-  });
-}
-if(item.classList.contains('githubURL')){
-  item.addEventListener('click', () => {
-    window.open('https://github.com/tonychicken/JavaScriptPractice/commits/main');
-  });
-}
+gitlink.forEach((item) => {
+  if (item.classList.contains('github')) {
+    item.addEventListener('click', () => {
+      window.open('https://github.com/tonychicken/JavaScriptPractice.git');
+    });
+  }
+  if (item.classList.contains('githubURL')) {
+    item.addEventListener('click', () => {
+      window.open('https://github.com/tonychicken/JavaScriptPractice/commits/main');
+    });
+  }
 
-if(item.classList.contains('vercel')){
-  item.addEventListener('click', () => {
-    window.open('https://java-script-practice-qb7i.vercel.app/');
-  });
-}
-})
-
-const CloseDropdown = ((item)=>{
-  if(ClassDemo.classList.contains('show')){
-    ClassDemo.classList.remove("show")
-  }else{
-    ClassDemo.classList.add("show");
+  if (item.classList.contains('vercel')) {
+    item.addEventListener('click', () => {
+      window.open('https://java-script-practice-qb7i.vercel.app/');
+    });
   }
 })
 
 
-// const ShowDemo = ((week)=>{
 
-//   const content = document.querySelector('#content');
+const ShowDemo = ((week) => {
+  if (ClassDemo.classList.contains('show')) {
+    ClassDemo.classList.remove("show")
+  } else {
+    ClassDemo.classList.add("show");
+  }
+  const content = document.querySelector('#content');
 
-//   switch (week) {
-//     case 'W1':
-//       content.innerHTML = `<iframe src="./W01/index.html" style="width: 100%; height: 800px;" ></iframe>`;
-//       break;
-//     case 'W1-MD':
-//       content.innerHTML = `<zero-md src="./MD/W01/W01.md"></zero-md>`;
-//       break;
-//     case 'W2':
-//       content.innerHTML = `<iframe src="./W02/tictactoe_06.html" style="width: 100%; height: 800px;" ></iframe>`;
-//       break;
-//     case 'W2-MD':
-//       content.innerHTML = `<zero-md src="./MD/W02/W02.md"></zero-md>`;
-//       break;
-//     case 'W3':
-//       content.innerHTML = `<iframe src="./W03/index.html" style="width: 100%; height: 800px;" ></iframe>`;
-//       break;
-//     case 'W3-MD':
-//       content.innerHTML = `<zero-md src="./MD/W03/W03.md"></zero-md>`;
-//       break;
-//     case 'W4':
-//       content.innerHTML = `<iframe src="./W04/index.html" style="width: 100%; height: 800px;" ></iframe>`;
-//       break;
-//     case 'W4-MD':
-//       content.innerHTML = `<zero-md src="./MD/W04/W04.md"></zero-md>`;
-//       break;
-//     case 'W5':
-//       content.innerHTML = `<iframe src="./W05/index.html" style="width: 100%; height: 800px;" ></iframe>`;
-//       break;
-//     case 'W5-MD':
-//       content.innerHTML = `<zero-md src="./MD/W05/W05.md"></zero-md>`;
-//       break;
-//     case 'W6':
-//       content.innerHTML = `<iframe src="./W06/index.html" style="width: 100%; height: 800px;" ></iframe>`;
-//       break;
-//     case 'W6-MD':
-//       content.innerHTML = `<zero-md src="./MD/W06/W06.md"></zero-md>`;
-//       break;
-//     default:
-//       content.innerHTML = `<h2>No content available for this week yet.</h2>`;
-//   }
+  switch (week) {
+    case 'W1':
+      content.innerHTML = `<iframe src="./W01/index.html" style="width: 100%; height: 800px;" ></iframe>`;
+      break;
+    case 'W1-MD':
+      content.innerHTML = `<zero-md src="./MD/W01/W01.md"></zero-md>`;
+      break;
+    case 'W2':
+      content.innerHTML = `<iframe src="./W02/tictactoe_06.html" style="width: 100%; height: 800px;" ></iframe>`;
+      break;
+    case 'W2-MD':
+      content.innerHTML = `<zero-md src="./MD/W02/W02.md"></zero-md>`;
+      break;
+    case 'W3':
+      content.innerHTML = `<iframe src="./W03/index.html" style="width: 100%; height: 800px;" ></iframe>`;
+      break;
+    case 'W3-MD':
+      content.innerHTML = `<zero-md src="./MD/W03/W03.md"></zero-md>`;
+      break;
+    case 'W4':
+      content.innerHTML = `<iframe src="./W04/index.html" style="width: 100%; height: 800px;" ></iframe>`;
+      break;
+    case 'W4-MD':
+      content.innerHTML = `<zero-md src="./MD/W04/W04.md"></zero-md>`;
+      break;
+    case 'W5':
+      content.innerHTML = `<iframe src="./W05/index.html" style="width: 100%; height: 800px;" ></iframe>`;
+      break;
+    case 'W5-MD':
+      content.innerHTML = `<zero-md src="./MD/W05/W05.md"></zero-md>`;
+      break;
+    case 'W6':
+      content.innerHTML = `<iframe src="./W06/index.html" style="width: 100%; height: 800px;" ></iframe>`;
+      break;
+    case 'W6-MD':
+      content.innerHTML = `<zero-md src="./MD/W06/W06.md"></zero-md>`;
+      break;
+    case 'W11':
+      content.innerHTML = `<iframe src="./W11/index.html" style="width: 100%; height: 800px;" ></iframe>`;
+      break;
+    case 'W11-MD':
+      content.innerHTML = `<zero-md src="./MD/W11/W11.md"></zero-md>`;
+      break;
+    case 'W12':
+      content.innerHTML = `<iframe src="./W12/index.html" style="width: 100%; height: 800px;" ></iframe>`;
+      break;
+    case 'W12-MD':
+      content.innerHTML = `<zero-md src="./MD/W12/W12.md"></zero-md>`;
+      break;
+    case 'W13':
+      content.innerHTML = `<iframe src="./W13/index.html" style="width: 100%; height: 800px;" ></iframe>`;
+      break;
+    case 'W13-MD':
+      content.innerHTML = `<zero-md src="./MD/W13/W13.md"></zero-md>`;
+      break;
+    case 'W14':
+      content.innerHTML = `<iframe src="./W14/index.html" style="width: 100%; height: 800px;" ></iframe>`;
+      break;
+    case 'W14-MD':
+      content.innerHTML = `<zero-md src="./MD/W14/W14.md"></zero-md>`;
+      break;
+    case 'W15':
+      content.innerHTML = `<iframe src="./W15/index.html" style="width: 100%; height: 800px;" ></iframe>`;
+      break;
+    case 'W15-MD':
+      content.innerHTML = `<zero-md src="./MD/W15/W15.md"></zero-md>`;
+      break;
+    case 'W16':
+      content.innerHTML = `<iframe src="./W16/index.html" style="width: 100%; height: 800px;" ></iframe>`;
+      break;
+    case 'W16-MD':
+      content.innerHTML = `<zero-md src="./MD/W16/W16.md"></zero-md>`;
+      break;
+    case 'W17':
+      content.innerHTML = `<iframe src="./W17/index.html" style="width: 100%; height: 800px;" ></iframe>`;
+      break;
+    case 'W17-MD':
+      content.innerHTML = `<zero-md src="./MD/W17/W17.md"></zero-md>`;
+      break;
+    case 'W18':
+      content.innerHTML = `<iframe src="./W18/index.html" style="width: 100%; height: 800px;" ></iframe>`;
+      break;
+    case 'W18-MD':
+      content.innerHTML = `<zero-md src="./MD/W18/W18.md"></zero-md>`;
+      break;
+    default:
+      content.innerHTML = `<h2>No content available for this week yet.</h2>`;
+  }
 
-// })
+})
 
 
 
