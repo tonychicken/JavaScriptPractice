4a706e4fc766b5e6edb202b810d1c1e3a7882894
tony.zhan <tony.zhan@shinda.com.tw>
2023-06-16 00:35:53 +0800

    update

--- midterm/W05/script_06.js
+++ midterm/W05/script_06.js
@@ -1,47 +1,32 @@
 'use strict';
 
-const modal1 = document.querySelector('.modal-1');
-const modal2 = document.querySelector('.modal-2');
-const modal3 = document.querySelector('.modal-3');
+const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnsCloseModal = document.querySelectorAll('.close-modal');
+const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log('modal', modal1);
-console.log('show-modal', btnsCloseModal);
-const ClassdemoOpenBtn = document.querySelector('#navbarDropdownClassDemo');
-const ClassDemo = document.querySelector('#dropdown_ClassDemo');
-console.log(ClassDemo)
-ClassdemoOpenBtn.addEventListener('click',()=>{
-  if(ClassDemo.classList.contains('show')){
-    ClassDemo.classList.remove("show")
-  }else{
-    ClassDemo.classList.add("show");
-  }
+console.log('modal', modal);
+console.log('show-modal', btnsOpenModal);
+
+
+const openModal = () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for(let i=0; i<btnsOpenModal.length; i++ ) {
+    btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
 });
-
-const openModal = (index) => {
-    if (index == 1) {
-        modal1.classList.remove('hidden');
-    } else if (index == 2) {
-        modal2.classList.remove('hidden');
-    } else if (index == 3) {
-        modal3.classList.remove('hidden');
-    }
-}
-
-const CloseModal = (index) => {
-    if (index == 1) {
-        modal1.classList.add('hidden');
-    } else if (index == 2) {
-        modal2.classList.add('hidden');
-    } else if (index == 3) {
-        modal3.classList.add('hidden');
-    }
-}
-
-for (let i = 0; i < btnsCloseModal.length; i++) {
-    btnsCloseModal[i].addEventListener('click', CloseModal(i));
-}
-for (let i = 0; i < btnsOpenModal.length; i++) {
-    btnsOpenModal[i].addEventListener('click', openModal(i));
-}
