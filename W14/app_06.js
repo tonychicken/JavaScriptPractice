aeceb66504894a89162086f4c536699f28419d1c
tony.zhan <tony.zhan@shinda.com.tw>
2023-06-15 16:26:42 +0800

    w14

--- W14/app_06.js
+++ W14/app_06.js
@@ -1,93 +1,28 @@
-const supabaseUrl  = 'https://mlybsphqojjqadpysauq.supabase.co'
-const supabaseKey  = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1seWJzcGhxb2pqcWFkcHlzYXVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgzNzgzMzYsImV4cCI6MTk5Mzk1NDMzNn0.MLaH9ADLG7fe2gVsXmiKOpsUwfi41lM7KlpFV2BiTx8'
+const btn = document.querySelector('.btn');
 
-let menu;
-
-const sectionCenter = document.querySelector('.section-center');
-const btnContainer = document.querySelector('.btn-container');
-
-const url = 'https://mlybsphqojjqadpysauq.supabase.co/rest/v1/tblmenu?select=*' 
-
-const displayMenuItems = (menu) => {
-  let displayMenu = menu.map((item) => {
-    return `
-      </article>
-      <article class="menu-item">
-        <img src=${item.Remote_img} alt=${item.title} class="photo">
-        <div class="item-info">
-          <header>
-            <h4>${item.title}</h4>
-            <h4 class="price">${item.price}</h4>
-          </header>
-          <p class="item-text">
-            ${item.descrip}
-          </p>
-        </div>
-      </article>  
-      `
-  })
-  displayMenu = displayMenu.join("");
-
-
-  sectionCenter.innerHTML = displayMenu;
-}
-
-const displayMenuButtons = () => {
-  console.log(menu)
-  const categories=['all', ...new Set(menu.map((item)=>item.category))];
-console.log(categories)
-  let menuBottons = categories.map((category) => {
-    return `<button type="button" class="filter-btn" data-id=${category}>${category}</button>`
-  });
-  menuBottons =menuBottons.join('');
-
-  console.log('categoryBtns before join', menuBottons);
-  btnContainer.innerHTML = menuBottons;
-
-
-  const filterBtns = document.querySelectorAll('.filter-btn')
-  console.log('menuBottons after join', filterBtns);
-  filterBtns.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-      console.log('data-id', e.currentTarget.dataset.id);
-      const category = e.currentTarget.dataset.id;
-      const menuCategory = menu.filter((menuItem) => {
-        if (menuItem.category === category) {
-          return menuItem;
-        }
-      });
-      if (category == 'all') {
-        displayMenuItems(menu);
-        console.log("filterMenu", menu);
-      } else {
-        displayMenuItems(menuCategory);
-        console.log("filterMenu", menuCategory);
-      }
-    });
-  });
-}
-
-const fetchdata = async () => {
+btn.addEventListener('click', async () => {
+  const url= 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a';
   try {
-// const response= await fetch(url);
-const response = await fetch(url,{
-  method:'GET',
-  headers: {
-    apikey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1seWJzcGhxb2pqcWFkcHlzYXVxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3ODM3ODMzNiwiZXhwIjoxOTkzOTU0MzM2fQ.aFW4YjCtnI8jU7DYIaNfR2wRNkDSvwZt0F2wSlvCcvQ",
-    Authorization:"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1seWJzcGhxb2pqcWFkcHlzYXVxIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3ODM3ODMzNiwiZXhwIjoxOTkzOTU0MzM2fQ.aFW4YjCtnI8jU7DYIaNfR2wRNkDSvwZt0F2wSlvCcvQ"
-  }
-})
-console.log(response)
-const data = await response.json();
-console.log(data)
-return data;
+    const response = await fetch(url);
+    
+    const result = await response.json();
+    console.log(result);
+    console.log(result.drinks);
+    displayItems(result.drinks)
   } catch (error) {
-console.log(error);
+    console.error(error);
   }
-}
-window.addEventListener('DOMContentLoaded', async () => {
-  menu = await fetchdata();
-  console.log(menu)
-  await displayMenuItems(menu);
-  await displayMenuButtons();
-})
+});
+
+
+const displayItems = (items) => {
+  const displayData = items
+    .map((item) => {
+      const  {strDrink}  = item;
+      return `<p>${strDrink} </p>`;
+    }).join('');
+  const element = document.createElement('div');
+  element.innerHTML = displayData;
+  document.body.appendChild(element)
+};
+
